eb8e9ccab9ba1d6f62817744c8771cd83c85caa7
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-23 16:50:27 +0100

    method_update_to_handle_invalid_input

--- java_bmi_update/src/main/java/dev/grigory/java_bmi_update/BMICalculator.java
+++ java_bmi_update/src/main/java/dev/grigory/java_bmi_update/BMICalculator.java
@@ -2,6 +2,9 @@ package dev.grigory.java_bmi_update;
 
 public class BMICalculator {
     public static double calculateBMI(double weight, double height) {
+        if (weight <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Weight and height must be greater than zero.");
+        }
         return Math.round((weight / (height * height)) * 100.0) / 100.0;
     }
 }
