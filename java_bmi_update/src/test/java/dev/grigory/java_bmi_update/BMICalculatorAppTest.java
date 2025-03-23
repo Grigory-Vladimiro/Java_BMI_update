03c8915b5d2a701e96aa85314f52d07830c278e6
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-23 17:35:18 +0100

    invalid_input_test_app

--- java_bmi_update/src/test/java/dev/grigory/java_bmi_update/BMICalculatorAppTest.java
+++ java_bmi_update/src/test/java/dev/grigory/java_bmi_update/BMICalculatorAppTest.java
@@ -1,13 +1,19 @@
 package dev.grigory.java_bmi_update;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class BMICalculatorAppTest {
-@Test
-public void testProcessBMI_Normal() {
+    @Test
+    public void testProcessBMI_Normal() {
     String result = BMICalculatorApp.processBMI(70, 1.75);
     assertTrue(result.contains("Normal weight"));
-}
+    }
+    @Test
+    public void testProcessBMI_InvalidInput() {
+    String result = BMICalculatorApp.processBMI(-10, 1.75);
+    assertEquals("Invalid input. Please enter valid numbers.", result);
+    }
 }
