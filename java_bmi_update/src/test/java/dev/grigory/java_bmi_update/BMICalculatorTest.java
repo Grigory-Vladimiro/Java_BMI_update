a14d051b2d2556edbdbf95eb8142edb8cfb4e3f6
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-23 16:46:20 +0100

    invalid_input_test

--- java_bmi_update/src/test/java/dev/grigory/java_bmi_update/BMICalculatorTest.java
+++ java_bmi_update/src/test/java/dev/grigory/java_bmi_update/BMICalculatorTest.java
@@ -1,12 +1,17 @@
 package dev.grigory.java_bmi_update;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class BMICalculatorTest {
-@Test
-public void testCalculateBMI_NormalCase() {
+    @Test
+    public void testCalculateBMI_NormalCase() {
     assertEquals(24.49, BMICalculator.calculateBMI(75, 1.75), 0.01);
-}
+    }
+    @Test
+    public void testCalculateBMI_InvalidInput() {
+    assertThrows(IllegalArgumentException.class, () -> BMICalculator.calculateBMI(-10, 1.75));
+    }
 }
