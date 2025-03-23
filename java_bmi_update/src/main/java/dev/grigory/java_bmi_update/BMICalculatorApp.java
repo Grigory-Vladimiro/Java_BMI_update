0f20bb90277b86a0908aa262914e6a8a2a0d0f63
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-23 17:40:58 +0100

    method_update_invalid_input_app

--- java_bmi_update/src/main/java/dev/grigory/java_bmi_update/BMICalculatorApp.java
+++ java_bmi_update/src/main/java/dev/grigory/java_bmi_update/BMICalculatorApp.java
@@ -2,8 +2,12 @@ package dev.grigory.java_bmi_update;
 
 public class BMICalculatorApp {
     public static String processBMI(double weight, double height) {
+    try {
         double bmi = BMICalculator.calculateBMI(weight, height);
         String category = BMICalculator.getBMICategory(bmi);
         return String.format("Your BMI: %.2f, Category: %s", bmi, category);
+    } catch (IllegalArgumentException e) {
+        return "Invalid input. Please enter valid numbers.";
+        }
     }
 }
