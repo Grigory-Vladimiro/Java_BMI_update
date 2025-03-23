88a21f86826adc011fa6b766b4df0c880a7d977c
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-23 17:14:05 +0100

    method_with_types_update

--- java_bmi_update/src/main/java/dev/grigory/java_bmi_update/BMICalculator.java
+++ java_bmi_update/src/main/java/dev/grigory/java_bmi_update/BMICalculator.java
@@ -8,7 +8,13 @@ public class BMICalculator {
         return Math.round((weight / (height * height)) * 100.0) / 100.0;
     }
     public static String getBMICategory(double bmi) {
+        if (bmi < 16) return "Severe Thinness";
+        if (bmi < 17) return "Moderate Thinness";
+        if (bmi < 18.5) return "Mild Thinness";
         if (bmi < 25) return "Normal weight";
-        return "Unknown";
+        if (bmi < 30) return "Overweight";
+        if (bmi < 35) return "Obesity class I";
+        if (bmi < 40) return "Obesity class II";
+        return "Obesity class III";
     }
 }
